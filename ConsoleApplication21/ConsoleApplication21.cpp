572401962a2a38e19677128c0c952f4d494793c3
Пользователь <@DESKTOP-LROMQDT>
2024-01-10 18:45:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication21/ConsoleApplication21.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+
+class Car {
+private:
+	std::string brand;
+	std::string model;
+	int year;
+	std::string color;
+	int mileage;
+
+public:
+	Car(std::string _brand, std::string _model, int _year, std::string _color, int _mileage)
+		: brand(_brand), model(_model), year(_year), color(_color), mileage(_mileage) {}
+
+	void printInfo() {
+		std::cout << "Brand: " << brand << std::endl;
+		std::cout << "Model: " << model << std::endl;
+		std::cout << "Year: " << year << std::endl;
+		std::cout << "Color: " << color << std::endl;
+		std::cout << "Mileage: " << mileage << " km" << std::endl;
+	}
+};
+
+int main() {
+	Car myCar("Toyota", "Camry", 2022, "Silver", 5000);
+	myCar.printInfo();
+
+	return 0;
+}
